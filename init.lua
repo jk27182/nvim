59376720cc01830a4d1f759beb4ce6c6bbfe30c2
Janik Königshofer <janik@koenigshofer-hh.de>
2023-02-08 14:17:03 +0100

    Git shortcuts und Speicher Shortcut

--- init.lua
+++ init.lua
@@ -65,6 +65,8 @@ require('packer').startup(function(use)
           require('toggleterm').setup()
   end}
 
+  use 'tpope/vim-unimpaired' -- Shortcuts for Buffer and file switching
+
   -- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
   local has_plugins, plugins = pcall(require, 'custom.plugins')
   if has_plugins then
@@ -142,6 +144,9 @@ vim.o.shiftwidth = 4
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- Save with space + s
+vim.keymap.set({ 'n', 'i' }, '<C-s>', "<cmd>w<CR>")
+
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
@@ -149,6 +154,11 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- git Shortcuts
+vim.keymap.set({'n', 'v'}, '<leader>gs', "<cmd>G status<CR>")
+vim.keymap.set({'n', 'v'}, '<leader>gd', "<cmd>G diff<CR>")
+vim.keymap.set({'n', 'v'}, '<leader>gg', "<cmd>G<CR>")
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
@@ -212,17 +222,19 @@ require('toggleterm').setup{
 
 -- Enable telescope fzf native, if installed
 pcall(require('telescope').load_extension, 'fzf')
-
+local function test()
+   return function ()
+      require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+      winblend = 10,
+      --previewer = false,
+    })
+   end
+end
 -- See `:help telescope.builtin`
+--vim.keymap.set('n', '<leader>/', "<cmd>Telescope current_buffer_fuzzy_find<CR> <bar> z.", { desc = '[/] Fuzzily search in current buffer]' })
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
 vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>/', function()
-  -- You can pass additional configuration to telescope to change theme, layout, etc.
-  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
-    previewer = false,
-  })
-end, { desc = '[/] Fuzzily search in current buffer]' })
+vim.keymap.set('n', '<leader>/', test(), { desc = '[/] Fuzzily search in current buffer]' })
 
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
@@ -230,7 +242,7 @@ vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { de
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 
--- [[ Configure Treesitter ]]
+-- [[ Configure Treesitter ]]]]
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
