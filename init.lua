89123a8a9b7f2a9293a0a0861d70054aecdec7ff
Janik Königshofer <janik@koenigshofer-hh.de>
2023-03-17 22:55:44 +0100

    Einfuegen von neogit fuer git client (magit)

--- init.lua
+++ init.lua
@@ -49,9 +49,12 @@ require('packer').startup(function(use)
   use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
 
   -- Git related plugins
-  use 'tpope/vim-fugitive'
-  use 'tpope/vim-rhubarb'
+  -- use 'tpope/vim-fugitive'
+  -- use 'tpope/vim-rhubarb'
   use 'lewis6991/gitsigns.nvim'
+  -- diffview fuer eine bessere Uebersicht von magit diff
+  use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
+  use { 'TimUntersberger/neogit', requires = {'nvim-lua/plenary.nvim', 'sindrets/diffview.nvim' } }
 
   -- Org mode in Neovim lazy loeading
   -- use {
@@ -350,6 +353,104 @@ require('packer').startup(function(use)
       vim.cmd([[nnoremap \ :Neotree reveal<cr>]])
     end
     }
+
+    require("neogit").setup {
+      disable_signs = false,
+      disable_hint = false,
+      disable_context_highlighting = false,
+      disable_commit_confirmation = false,
+      -- Neogit refreshes its internal state after specific events, which can be expensive depending on the repository size.
+      -- Disabling `auto_refresh` will make it so you have to manually refresh the status after you open it.
+      auto_refresh = true,
+      -- Value used for `--sort` option for `git branch` command
+      -- By default, branches will be sorted by commit date descending
+      -- Flag description: https://git-scm.com/docs/git-branch#Documentation/git-branch.txt---sortltkeygt
+      -- Sorting keys: https://git-scm.com/docs/git-for-each-ref#_options
+      sort_branches = "-committerdate",
+      disable_builtin_notifications = false,
+      use_magit_keybindings = false,
+      -- Change the default way of opening neogit
+      kind = "tab",
+      -- The time after which an output console is shown for slow running commands
+      console_timeout = 2000,
+      -- Automatically show console if a command takes more than console_timeout milliseconds
+      auto_show_console = true,
+      -- Persist the values of switches/options within and across sessions
+      remember_settings = true,
+      -- Scope persisted settings on a per-project basis
+      use_per_project_settings = true,
+      -- Array-like table of settings to never persist. Uses format "Filetype--cli-value"
+      --   ie: `{ "NeogitCommitPopup--author", "NeogitCommitPopup--no-verify" }`
+      ignored_settings = {},
+      -- Change the default way of opening the commit popup
+      commit_popup = {
+        kind = "split",
+      },
+      -- Change the default way of opening the preview buffer
+      preview_buffer = {
+        kind = "split",
+      },
+      -- Change the default way of opening popups
+      popup = {
+        kind = "split",
+      },
+      -- customize displayed signs
+      signs = {
+        -- { CLOSED, OPENED }
+        section = { ">", "v" },
+        item = { ">", "v" },
+        hunk = { "", "" },
+      },
+      integrations = {
+        -- Neogit only provides inline diffs. If you want a more traditional way to look at diffs, you can use `sindrets/diffview.nvim`.
+        -- The diffview integration enables the diff popup, which is a wrapper around `sindrets/diffview.nvim`.
+        --
+        -- Requires you to have `sindrets/diffview.nvim` installed.
+        -- use {
+        --   'TimUntersberger/neogit',
+        --   requires = {
+        --     'nvim-lua/plenary.nvim',
+        --     'sindrets/diffview.nvim'
+        --   }
+        -- },
+        --
+        diffview = true
+      },
+      -- Setting any section to `false` will make the section not render at all
+      sections = {
+        untracked = {
+          folded = false
+        },
+        unstaged = {
+          folded = false
+        },
+        staged = {
+          folded = false
+        },
+        stashes = {
+          folded = true
+        },
+        unpulled = {
+          folded = true
+        },
+        unmerged = {
+          folded = false
+        },
+        recent = {
+          folded = true
+        },
+      },
+      -- override/add mappings
+      mappings = {
+        -- modify status buffer mappings
+        status = {
+          -- Adds a mapping with "B" as key that does the "BranchPopup" command
+          ["B"] = "BranchPopup",
+          -- Removes the default mapping of "s"
+          ["s"] = "",
+        }
+      }
+    }
   use 'navarasu/onedark.nvim' -- Theme inspired by Atom
   use 'nvim-lualine/lualine.nvim' -- Fancier statusline
   use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
@@ -397,6 +498,8 @@ vim.api.nvim_create_autocmd('BufWritePost', {
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
+-- Das bei Split das Fenster immer 'below' statt above ist
+vim.o.splitbelow = true
 
 -- Set highlight on search
 vim.o.hlsearch = false
@@ -461,7 +564,19 @@ vim.keymap.set({'n','v'}, '<leader>P', '+P')
 -- git Shortcuts
 vim.keymap.set({'n', 'v'}, '<leader>gs', "<cmd>G status<CR>")
 vim.keymap.set({'n', 'v'}, '<leader>gd', "<cmd>G diff<CR>")
-vim.keymap.set({'n', 'v'}, '<leader>gg', "<cmd>G<CR>")
+-- vim.keymap.set({'n', 'v'}, '<leader>gg', "<cmd>G<CR>")
+
+local function getDir(path)
+  return path:match("^(.-)[\\/][^\\/]-$")
+end
+-- Git status mit magit im Dir von dem File des Buffers
+vim.keymap.set({'n', 'v'}, '<leader>gg', function ()
+  require('neogit').open({
+    cwd=getDir(vim.api.nvim_buf_get_name(0))
+  })
+end)
+-- Git status mit magit im Dir wo nvim im Terminal gestartet wurde
+vim.keymap.set({'n', 'v'}, '<leader>gG', function () require('neogit').open() end)
 
 -- Behalten der Selection in visual mode
 vim.keymap.set('v', '<', '<gv')
